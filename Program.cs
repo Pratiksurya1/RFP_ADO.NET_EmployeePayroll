db74e90dae9b9ac2b03f9fd82fc412c4c3376dab
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-02-24 09:53:41 +0530

    wellcome

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_EmployeePayroll
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("****************** WELLCOME ******************");
+
+        }
+    }
+}
